707005f9728639e3cfe093485e687399fe44594c
sora <shinpei.okamura@insprout.com>
2012-11-18 03:34:52 +0900

    add an import

--- sazabi/fabric/util.py
+++ sazabi/fabric/util.py
@@ -3,6 +3,8 @@
 from string import Template
 from StringIO import StringIO
 
+from fabric.api import put
+
 def upload_template(filename, destination, context, mode=None):
     """
     Create file from string.Template and upload it.
